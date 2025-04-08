e2e38d7cd52f4cd5956aecd0bf0a88379023d114
Chokapy <cedric.jankie@gmail.com>
2025-04-08 18:39:03 +0200

    Python init

---
+++ randomizer_backend.py
@@ -0,0 +1,7 @@
+"""
+Program name : randomizer_backend.py
+Author : Chokapi
+Date : 08.04.2025
+Modif : 08.04.2025
+Version : 2.0
+"""
---
+++ randomizer_main.py
@@ -0,0 +1,8 @@
+"""
+Program name : randomizer_main.py
+Author : Chokapi
+Date : 08.04.2025
+Modif : 08.04.2025
+Version : 2.0
+"""
+import randomizer_frontend as frnd
