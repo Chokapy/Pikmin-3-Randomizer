26112c596480203da6540a238267ee71fcded468
Chokapy <cedric.jankie@gmail.com>
2026-06-14 11:12:39 +0200

    random music

--- randomizer_backend.py
+++ randomizer_backend.py
@@ -14,6 +14,7 @@ import sarc
 
 base_path = "mod-file"
 generator_folder = "generator"
+music_folder = "stream"
 
 ##-----path/file handler-----##
 def get_list(path):
@@ -96,7 +97,8 @@ def pack_all():
 
 
 ##-----random function-----##
-def randomize_file(sarc_path, search_pool, random_pool):
+#random mean that any possible combination can be made
+def randomize_objects(sarc_path, search_pool, random_pool):
     """
     randomize_file randomize a .sarc file
     :param sarc_path: .sarc file path
@@ -131,7 +133,8 @@ def randomize_file(sarc_path, search_pool, random_pool):
     with open(sarc_path, "wb") as f:
         writer.write(f)
 
-def randomize_all(data_file_path):
+
+def randomize_all_objects(data_file_path):
     """
     randomize_all randomize all .sarc files
     :param data_file_path: the pool data
@@ -143,7 +146,107 @@ def randomize_all(data_file_path):
             if filename.endswith(".sarc"):
                 file_path = os.path.join(dirpath, filename)
 
-                randomize_file(file_path, data["search_pool"], data["random_pool"])
+                print(f"Randomizing : {file_path}")
+
+                randomize_objects(file_path, data["search_pool"], data["random_pool"])
+
+
+def randomize_params(sarc_path, param_name, values):
+    #TODO being able the randomize a param
+    pass
+
+
+def randomize_all_params(data_file_path):
+    #TODO being able the randomize all params
+    pass
+
+
+##-----shuffle function-----##
+#shufle mean that each element appear once
+def shuffle_objects(sarc_path, search_pool, random_pool):
+    """
+    randomize_file randomize a .sarc file
+    :param sarc_path: .sarc file path
+    :param search_pool: objet that will be randomized
+    :param random_pool: objet that will be randomized into
+    """
+    with open(sarc_path, "rb") as f:
+        archive = sarc.read_file_and_make_sarc(f)
+
+    writer = sarc.SARCWriter(be=False)
+
+    count = 0
+
+    for file_name in archive.list_files():
+        data = bytes(archive.get_file_data(file_name)).splitlines(True)
+
+        for i, line in enumerate(data):
+            for search in search_pool:
+                if f"\"{search}\"".encode() in line:
+                    line = line.replace(
+                        f"\"{search}\"".encode(),
+                        f"\"{random.choice(random_pool)}\"".encode()
+                    )
+                    count += 1
+
+            data[i] = line
+
+        writer.add_file(file_name, b"".join(data))
+
+    print(f"Replaced {count} occurrences")
+
+    with open(sarc_path, "wb") as f:
+        writer.write(f)
+
+
+def shuffle_all_objects(data_file_path):
+    """
+    randomize_all randomize all .sarc files
+    :param data_file_path: the pool data
+    """
+    data = get_list(data_file_path)
+    for dirpath, dirnames, filenames in os.walk(generator_path):
+        for filename in filenames:
+
+            if filename.endswith(".sarc"):
+                file_path = os.path.join(dirpath, filename)
+
+                print(f"Randomizing : {file_path}")
+
+                randomize_objects(file_path, data["search_pool"], data["random_pool"])
+
+
+def shuffle_params(sarc_path, param_name, values):
+    #TODO being able the randomize a param
+    pass
+
+
+def shuffle_all_params(data_file_path):
+    #TODO being able the randomize all params
+    pass
+
+
+def shuffle_files(dir):
+    files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+    original_names = [os.path.splitext(f)[0] for f in files]
+    extensions = [os.path.splitext(f)[1] for f in files]
+
+    # Shuffle the names
+    shuffled_names = original_names[:]
+    random.shuffle(shuffled_names)
+
+    # Rename to temporary names to avoid overwriting
+    temp_names = []
+    for i, f in enumerate(files):
+        temp_name = f"__tempfile_{i}__{extensions[i]}"
+        os.rename(os.path.join(dir, f), os.path.join(dir, temp_name))
+        temp_names.append(temp_name)
+
+    # Rename temporary files to shuffled names
+    for temp_file, new_base in zip(temp_names, shuffled_names):
+        new_name = new_base + os.path.splitext(temp_file)[1]
+        os.rename(os.path.join(dir, temp_file), os.path.join(dir, new_name))
+        print(f"{temp_file} → {new_name}")
 
 
 ##-----program-----##
@@ -153,8 +256,13 @@ b = f"{generator_path}".encode()
 
 unpack_all()
 
-randomize_all("data/randomizerData/Enemies.json")
-randomize_all("data/randomizerData/Fruits.json")
-randomize_all("data/randomizerData/Plants.json")
+print("#-----Enemies-----#")
+randomize_all_objects("data/randomizerData/Enemies.json")
+
+print("#-----Fruits-----#")
+randomize_all_objects("data/randomizerData/Fruits.json")
 
 pack_all()
+
+music_path = get_folder(base_path, music_folder)
+shuffle_files(music_path)
